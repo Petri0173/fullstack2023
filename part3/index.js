5ea3d3e9eaeb4f3da64247d7dbe3284c19839241
pemakani <pemakani@jyu.fi>
2023-07-17 14:08:06 +0300

    Update index.js teht. 3.6

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
     {
         id: 1,
@@ -45,6 +47,50 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end();
     }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    persons = persons.filter(person => person.id === id);
+    response.status(204).end();
+});
+
+const randomId = () => {
+    let randomId = Math.floor(Math.random() * 1000) + 5;
+    return randomId;
+};
+
+app.post('/api/persons', (request, response) => {
+    const body= request.body;
+
+    if (!body.name) {
+        return response.status(400).json({ 
+            error: 'name missing' 
+        });
+    }
+
+    if (!body.number) {
+        return response.status(400).json({ 
+            error: 'number missing' 
+            });
+    }
+
+    const nameExists = persons.some((person) => person.name === body.name);
+
+        if (nameExists) {
+            return response.status(400).json({
+                error: 'name already exists'
+            });
+        }
+
+    const person = {
+        id: randomId(),
+        name: body.name,
+        number: body.number,
+    };
+
+    persons = persons.concat(person);
+    response.json(person);
 });
 
 const PORT = 3001;
